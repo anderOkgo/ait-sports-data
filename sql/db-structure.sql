a258f0b9ea48b961b678a0527fda3bc09cccce02
anderson rangel <andergroundmsg@gmail.com>
2023-11-08 14:11:53 +1000

    modify initial structure

--- sql/db-structure.sql
+++ sql/db-structure.sql
@@ -1,11 +1,11 @@
 -- ----------------------------------------------database-
 
-CREATE DATABASE IF NOT EXISTS animecre_finan;
+CREATE DATABASE IF NOT EXISTS animecre_ait-sports;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------------------------tables---
 
-USE animecre_finan;
+USE animecre_ait-sports;
 
 DROP TABLE IF EXISTS `matches`;
 CREATE TABLE `matches` (
@@ -21,37 +21,7 @@ CREATE TABLE `matches` (
 -- ----------------------------------------------views---
 
 DROP VIEW IF EXISTS view_moviments; -- ----------
-CREATE OR REPLACE VIEW view_moviments AS
-SELECT
-    m.id,
-    m.name,
-    m.value AS val,
-    tag,
-    ts.name AS 'source',
-    LEFT(DATE_FORMAT(m.date_moviment, '%Y-%m-%d %H:%i:%s'),16) AS datemov
-FROM
-    moviments m
-JOIN type_sources ts ON
-    m.type_source_id = ts.id
-ORDER BY
-    m.date_moviment DESC
-LIMIT 100;
 
 
 -- --------------------------------------------------- Procedures
 DROP PROCEDURE IF EXISTS `proc_insert_moviment`;
-DELIMITER $$
-CREATE  PROCEDURE proc_insert_moviment  (
-  IN p_name VARCHAR(255),
-  IN p_value DECIMAL(10,2),
-  IN p_date DATETIME,
-  IN p_type_source_id INT,
-  IN p_tag VARCHAR(150)
-  ) SQL SECURITY INVOKER
-BEGIN
-    INSERT INTO animecre_finan.moviments
-    (name, value, date_moviment, type_source_id, tag)
-    VALUES (p_name, p_value, p_date, p_type_source_id, p_tag);
-END
-$$
-DELIMITER ;
