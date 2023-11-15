023288e8cf2fe7a91a386b9e613318e517d80762
anderson rangel <andergroundmsg@gmail.com>
2023-11-15 14:17:41 +1000

    change bat and ignere files

--- com/db-local-data-backup.bat
+++ com/db-local-data-backup.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 SETLOCAL
 IF EXIST setter.bat (CD.. & CALL com\setter.bat) ELSE ( CALL com\setter.bat )
-C:\xampp\mysql\bin\mysqldump.exe -u %MysqlUserLocal.txt% -h %MysqlHost.txt% %MysqlDBLocal.txt% --no-create-info --complete-insert --default-character-set=utf8 --skip-triggers --compact --no-create-db>%FTPFolder.txt%/db-data.sql
+%Mysqlpath.txt%  -u %MysqlUserLocal.txt% -h %MysqlHost.txt% %MysqlDBLocal.txt% --no-create-info --complete-insert --default-character-set=utf8 --skip-triggers --compact --no-create-db>%FTPFolder.txt%/db-data.sql
 PAUSE
--- com/db-local-restore-swap.bat
+++ com/db-local-restore-swap.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 SETLOCAL
 IF EXIST setter.bat (CD.. & CALL com\setter.bat) ELSE ( CALL com\setter.bat )
-CMD /c C:\xampp\mysql\bin\mysql.exe -u %MysqlUserLocal.txt% -h %MysqlHost.txt% %MysqlDBLocal.txt% --default-character-set=utf8<sql\db-swap.sql
+CMD /c %Mysqlpath.txt% -u %MysqlUserLocal.txt% -h %MysqlHost.txt% %MysqlDBLocal.txt% --default-character-set=utf8<sql\db-swap.sql
 PAUSE
